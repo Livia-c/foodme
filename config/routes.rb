c64dead3bfa68d6212fe4f1c193e50da718089c3
Daniel Stadelmann <daniel.stadelmann.info@gmail.com>
2022-11-30 17:53:43 +0100

    orders and menu item mobile

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
   resources :orders
   resources :menu_item_customers
 
-  post 'order_items/:id/add' => "order_items#add_quantity", as: "order_item_add"
-  post 'order_items/:id/reduce' => "order_items#reduce_quantity", as: "order_item_reduce"
+  # post 'order_items/:id/add' => "order_items#add_quantity", as: "order_item_add"
+  # post 'order_items/:id/reduce' => "order_items#reduce_quantity", as: "order_item_reduce"
 end

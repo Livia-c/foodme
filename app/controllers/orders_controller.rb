0a76fc8baba99c57d4ddbbad358c74021c93ca07
Daniel Stadelmann <daniel.stadelmann.info@gmail.com>
2022-12-01 15:00:30 +0100

    fixed order status and logic

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,10 @@ class OrdersController < ApplicationController
     @orders = Order.where(user: current_user)
   end
 
+  def new
+    @order = Order.new
+  end
+
   def show
     @order = @current_order
     @order = Order.find(params[:id])
@@ -20,13 +24,18 @@ class OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    @order.status == "pending" ? @order.update(status: 1) : @order.update(status: 2)
+    # @order.status == "pending" ? @order.update(status: 2) : @order.update(status: 3)
     # another way to write the code above
-    # @order.pending? ? @order.in_progress! : @order.delivered!
+    if @order.waiting?
+      @order.pending!
+      @order.active = false
+    else
+      @order.pending? ? @order.in_progress! : @order.delivered!
+    end
     if @order.save
       redirect_to order_items_path
     end
-   end
+  end
 
   # private
   # redirect_to order_path(@current_order), notice: "Item was successfully removed from order"

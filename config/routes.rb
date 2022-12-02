3216d4289720e353c243b388e515250b501282a8
Livia C <lcebotarescu@gmail.com>
2022-12-02 10:26:43 +0100

    New landing page after order was sent to kitchen

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,6 @@ Rails.application.routes.draw do
   get '/order_items/completed_orders', to: 'order_items#completed_orders', as: 'completed_orders'
   post 'order_items/:id/add' => "order_items#add_quantity", as: "order_item_add"
   post 'order_items/:id/reduce' => "order_items#reduce_quantity", as: "order_item_reduce"
+  get '/order/:id/order_placed', to: 'orders#order_placed', as: 'placed'
 
 end

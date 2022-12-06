428da63c6210db9177f24d48f199976796299fbc
Livia C <lcebotarescu@gmail.com>
2022-12-06 14:45:47 +0100

    fixed seed and display of table nr

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
   has_many :menu_items, through: :order_items
   belongs_to :user
 
-  TABLE = (1..10)
+  TABLE = (0..10)
   validates_inclusion_of :table_number, in: TABLE, on: :update
   validates :table_number, presence: true,  on: :update
 

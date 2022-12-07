b2800d825a1b02815a6777234f090e9b0fd1700b
Livia C <lcebotarescu@gmail.com>
2022-12-07 21:15:10 +0100

    update live kithcen and orders incl table nr

--- app/javascript/controllers/livekitchen_subscription_controller.js
+++ app/javascript/controllers/livekitchen_subscription_controller.js
@@ -3,14 +3,13 @@ import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="livekitchen-subscription"
 export default class extends Controller {
-  static values = { chatroomId: Number }
   static targets = ["orders"]
 
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel: "LivekitchenChannel"},
       // { received: data => console.log(data)}
-      { received: data => this.ordersTarget.insertAdjacentHTML("beforeend", data) }
+      { received: data => this.ordersTarget.innerHTML = data }
     )
   }
 }

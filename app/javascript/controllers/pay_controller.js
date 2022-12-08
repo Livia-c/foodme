e17f7ff3bb76abc6954d4903b240f970435c2f0b
Livia C <lcebotarescu@gmail.com>
2022-12-08 09:58:04 +0100

    morning review bug fixes

--- app/javascript/controllers/pay_controller.js
+++ app/javascript/controllers/pay_controller.js
@@ -5,6 +5,10 @@ export default class extends Controller {
   static values = {key: String, sessionId: String}
   connect() {
   // }
+
+  // !!!!!!
+  // this code is obsolete, we switched to rails redirection in the controller
+
   // click() {
   //     const stripe = Stripe(this.keyValue);
   //     stripe.redirectToCheckout({
